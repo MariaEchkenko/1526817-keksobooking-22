9c1a441aab5e1c7993dbb5d52ae389eb4b8112e7
Maria Echkenko <mashasly@mail.ru>
2021-02-06 01:31:00 +0300

    Откорректировала замечания по функциям

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона включительно
+ * @param {number} min - минимальное значение
+ * @param {number} max - максимальное значение
+ * @return {number|null} - случайное число
+ */
 
 const getRandomInteger = function (min, max) {
-  let minNumber = Math.ceil(min);
-  let maxNumber = Math.floor(max);
-  if (min<0 || max<0 || (minNumber>max && maxNumber<min)) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0 || (min > max)) {
     return null
   }
-  if (minNumber == maxNumber) {
-    return minNumber
+  if (min == max) {
+    return min
   }
 
-  //Пример функции взят с MDN https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  let randomInteger = Math.floor(Math.random() * (maxNumber + 1 - minNumber) + minNumber);
+  let randomInteger = Math.floor(Math.random() * (max + 1 - min) + min);
   return Math.floor(randomInteger);
 }
 
 getRandomInteger(1,5);
 
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+/**
+ * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+ * @param {number} min - минимальное значение
+ * @param {number} max - максимальное значение
+ * @param {number} n - количество знаков после запятой
+ * @return {number|null} - случайное число
+ */
 
 const getRandomDecimalNumber = function (min, max, n) {
-  if (min<0 || max<0) {
+  if (min < 0 || max < 0) {
     return null
   }
   if (min == max) {
     return +min.toFixed(n)
   }
   let randomNumber = Math.random() * (max - min) + min;
-  return +(randomNumber.toFixed(n));
+  return +randomNumber.toFixed(n);
 }
 
 getRandomDecimalNumber(1.1,1.6,3);
